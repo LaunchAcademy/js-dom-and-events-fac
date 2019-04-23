711e0bb69336e4faec9b552b27e7416e9e16eb0b
Nicholas Alberts <nwalberts5@gmail.com>
2019-04-23 18:11:20 -0400

    start

---
+++ README.md
@@ -0,0 +1,16 @@
+### General Notes
+
+<!-- HTML -->
+Via Inspector
+* editing html tags, content, class names and ids
+
+<!-- CSS -->
+* Introduction to CSS (again for them)
+* 5-10 standard styles you may use
+* hard refreshing
+
+* Using the Chrome Inspector
+- Box Model (brief)
+- show how less specific styles get “crossed out"
+- modifying css properties
+- computed styles tab
---
+++ main.js
@@ -0,0 +1,27 @@
+// Define event listener
+// let alertButton = document.getElementById('alert-button')
+//
+//
+// let alertCallbackFunction = () => {
+//   alert("We were clicked!")
+// }
+//
+// alertButton.addEventListener("click", alertCallbackFunction)
+
+// add alert callback, or a console.log callback
+
+// seperate callback function
+
+// change an element's text
+
+// change to a callback that hides the element
+
+// Have a simple form, and console.log as they input
+
+// let characterField = document.getElementById('character')
+//
+// let inputCallback = (event) => {
+//   console.log(event.target.value)
+// }
+//
+// characterField.addEventListener("change", inputCallback)
